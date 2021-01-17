9b49b760e4ee477ea618d718c07479f14707e3fe
Shivam Runthala <runthalashivam@gmail.com>
2021-01-17 19:15:50 +0530

    Added the postlisst page

---
+++ scripts/postslist.js
@@ -0,0 +1,21 @@
+function showTrashModal(id){
+  console.log(id);
+  var modal = document.getElementsByClassName("trash-modal")[0];
+  modal.style.display = "block";
+
+
+  var deletePostBtn = document.getElementsByClassName("trash-modal-button-yes")[0];
+  deletePostBtn.onclick = function() {
+    document.getElementById(id).remove();
+    modal.style.display = "none";
+  }
+}
+
+function hideTrashModal(){
+  var modal = document.getElementsByClassName("trash-modal")[0];
+  modal.style.display = "none";
+}
+
+function navigateToPost() {
+  location.href = "post.html";
+}
